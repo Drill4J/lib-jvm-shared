b3014b8ea3a100ff09d16f01340402bc8b0d9854
iryabov <to.iryabov@gmail.com>
2023-06-20 10:52:42 +0200

    feat: add native method getting package prefixes EPMDJ-10610

--- plugin-api-agent/src/jvmMain/kotlin/com/epam/drill/plugin/api/Native.kt
+++ plugin-api-agent/src/jvmMain/kotlin/com/epam/drill/plugin/api/Native.kt
@@ -25,4 +25,5 @@ object Native {
     external fun RetransformClasses(count: Int, classes: Array<Class<*>>)
     external fun RetransformClassesByPackagePrefixes(prefixes: ByteArray): Int
     external fun GetAllLoadedClasses(): Array<Class<*>>
+    external fun GetPackagePrefixes(): String
 }
