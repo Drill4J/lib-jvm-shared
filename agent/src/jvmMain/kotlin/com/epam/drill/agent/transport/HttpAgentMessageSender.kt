e4c55664068ea6205873f94b3c6d0a565364285f
epamrsa <epamrsa@gmail.com>
2023-12-08 21:50:38 +0300

    feat: Migrate java-agent to REST
    - Adjusted requests paths
    - Implemented GZIP support
    - Implemented HTTPS support
    
    Refs: EPMDJ-10729

--- agent/src/jvmMain/kotlin/com/epam/drill/agent/transport/HttpAgentMessageSender.kt
+++ agent/src/jvmMain/kotlin/com/epam/drill/agent/transport/HttpAgentMessageSender.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020 - 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.epam.drill.agent.transport
 
 import kotlin.concurrent.thread
@@ -14,15 +29,16 @@ object HttpAgentMessageSender: AgentMessageSender {
 
     private val logger = KotlinLogging.logger {}
 
-    private var agentInstanceId = WsConfiguration.getInstanceId()
     private var attached = false
+    private val agentId = WsConfiguration.getAgentId()
+    private val buildVersion = WsConfiguration.getBuildVersion()
 
     override fun isTransportAvailable(): Boolean = attached
 
     override fun <T: AgentMessage> send(destination: AgentMessageDestination, message: T): ResponseStatus {
         val status = when(destination.type) {
-            "POST" -> HttpClient.post("agent/$agentInstanceId/${destination.target}", message)
-            "PUT" -> HttpClient.put("agent/$agentInstanceId/${destination.target}", message)
+            "POST" -> HttpClient.post("agents/$agentId/builds/$buildVersion/${destination.target}", message)
+            "PUT" -> HttpClient.put("agents/$agentId/builds/$buildVersion/${destination.target}", message)
             else -> -1
         }
         return HttpResponseStatus(status)
