cccb7d892e11b1253956e82f1921b4bb45f3a988
epamrsa <epamrsa@gmail.com>
2024-01-03 15:04:08 +0300

    feat: Review configuration classes, try to remove custom URL class
    Added license headers
    
    Refs: EPMDJ-10659

--- agent-config/src/nativeMain/kotlin/com/epam/drill/agent/configuration/provider/InstallationDirProvider.kt
+++ agent-config/src/nativeMain/kotlin/com/epam/drill/agent/configuration/provider/InstallationDirProvider.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020 - 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.epam.drill.agent.configuration.provider
 
 import kotlinx.cinterop.toKString
@@ -8,7 +23,7 @@ import com.epam.drill.agent.configuration.DefaultAgentConfiguration
 
 class InstallationDirProvider(
     private val configurationProviders: Set<AgentConfigurationProvider>,
-    override val priority: Int = 100
+    override val priority: Int = 300
 ) : AgentConfigurationProvider {
 
     private val pathSeparator = if (Platform.osFamily == OsFamily.WINDOWS) "\\" else "/"
--- agent-config/src/nativeMain/kotlin/com/epam/drill/agent/configuration/provider/PropertiesFileProvider.kt
+++ agent-config/src/nativeMain/kotlin/com/epam/drill/agent/configuration/provider/PropertiesFileProvider.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020 - 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.epam.drill.agent.configuration.provider
 
 import kotlinx.cinterop.memScoped
@@ -11,7 +26,7 @@ import platform.posix.open
 
 class PropertiesFileProvider(
     private val configurationProviders: Set<AgentConfigurationProvider>,
-    override val priority: Int = 300
+    override val priority: Int = 200
 ) : AgentConfigurationProvider {
 
     private val pathSeparator = if (Platform.osFamily == OsFamily.WINDOWS) "\\" else "/"
