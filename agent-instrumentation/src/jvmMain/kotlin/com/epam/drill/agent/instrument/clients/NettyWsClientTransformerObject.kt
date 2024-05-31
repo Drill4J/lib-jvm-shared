17139439841b206ce6cd1ce8012b2241dab9f29a
epamrsa <epamrsa@gmail.com>
2024-05-31 18:50:15 +0300

    feat: Implement support for WebSocket connections
    - Implemented transformer for Netty client
    
    Refs: EPMDJ-10809

--- agent-instrumentation/src/jvmMain/kotlin/com/epam/drill/agent/instrument/clients/NettyWsClientTransformerObject.kt
+++ agent-instrumentation/src/jvmMain/kotlin/com/epam/drill/agent/instrument/clients/NettyWsClientTransformerObject.kt
@@ -20,31 +20,57 @@ import javassist.CtClass
 import mu.KotlinLogging
 import com.epam.drill.agent.instrument.AbstractTransformerObject
 import com.epam.drill.agent.instrument.HeadersProcessor
+import com.epam.drill.agent.instrument.servers.NettyTransformerObject
 
 abstract class NettyWsClientTransformerObject : HeadersProcessor, AbstractTransformerObject() {
 
     override val logger = KotlinLogging.logger {}
 
     override fun permit(className: String?, superName: String?, interfaces: Array<String?>) =
-        "io/netty/handler/codec/http/websocketx/WebSocketClientHandshaker" == superName
+        listOf(
+            "io/netty/bootstrap/Bootstrap",
+            "io/netty/handler/codec/http/websocketx/WebSocketClientHandshaker"
+        ).contains(className)
 
     override fun transform(className: String, ctClass: CtClass) {
-        logger.info { "transform: Starting NettyWsClientTransformer..." }
-        ctClass.getMethod("newHandshakeRequest", "()Lio/netty/handler/codec/http/FullHttpRequest;")
-            .insertCatching(
+        logger.info { "transform: Starting NettyWsTransformerObject for $className..." }
+        when (className) {
+            "io/netty/bootstrap/Bootstrap" -> transformBootstrap(ctClass)
+            "io/netty/handler/codec/http/websocketx/WebSocketClientHandshaker" -> transformClientHandshaker(ctClass)
+        }
+    }
+
+    private fun transformBootstrap(ctClass: CtClass) = ctClass
+        .getMethod("doResolveAndConnect", "(Ljava/net/SocketAddress;Ljava/net/SocketAddress;)Lio/netty/channel/ChannelFuture;")
+        .insertCatching(
             CtBehavior::insertAfter,
             """
-            if (${this::class.java.name}.INSTANCE.${this::hasHeaders.name}()) { 
-                java.util.Map headers = ${this::class.java.name}.INSTANCE.${this::retrieveHeaders.name}();
-                java.util.Iterator iterator = headers.entrySet().iterator();             
-                while (iterator.hasNext()) {
-                    java.util.Map.Entry entry = (java.util.Map.Entry) iterator.next();
-                    ${'$'}_.headers().set((String) entry.getKey(), entry.getValue());
-                }
-                ${this::class.java.name}.INSTANCE.${this::logInjectingHeaders.name}(headers);
+            if(${this::class.java.name}.INSTANCE.${this::hasHeaders.name}()) {
+                java.util.Map drillHeaders = ${this::class.java.name}.INSTANCE.${this::retrieveHeaders.name}();
+                io.netty.util.AttributeKey drillContextKey = io.netty.util.AttributeKey.valueOf("${NettyTransformerObject.DRILL_CONTEXT_KEY}");
+                ${'$'}_.channel().attr(drillContextKey).set(drillHeaders);
             }
             """.trimIndent()
         )
-    }
+
+    private fun transformClientHandshaker(ctClass: CtClass) = ctClass
+        .getMethod("handshake", "(Lio/netty/channel/Channel;Lio/netty/channel/ChannelPromise;)Lio/netty/channel/ChannelFuture;")
+        .insertCatching(
+            CtBehavior::insertBefore,
+            """
+            io.netty.util.AttributeKey drillContextKey = io.netty.util.AttributeKey.valueOf("${NettyTransformerObject.DRILL_CONTEXT_KEY}");                                            
+            io.netty.util.Attribute drillContextAttr = $1.attr(drillContextKey);
+            java.util.Map drillHeaders = (java.util.Map) drillContextAttr.get();
+            if (drillHeaders != null) {
+                java.util.Iterator iterator = drillHeaders.entrySet().iterator();
+                while (iterator.hasNext()) {
+                     java.util.Map.Entry entry = (java.util.Map.Entry) iterator.next();
+                     if (!customHeaders.contains((String) entry.getKey())) {
+                         customHeaders.add((String) entry.getKey(), entry.getValue());
+                     }
+                }                    
+            }
+            """.trimIndent()
+        )
 
 }
