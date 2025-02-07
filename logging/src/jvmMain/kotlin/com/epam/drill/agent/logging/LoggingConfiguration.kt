d156fc963b6c33fba3f653479dddcc767656b4ad
iryabov <to.iryabov@gmail.com>
2025-02-07 12:59:25 +0100

    fix: set message limit by default for the logger configuration

--- logging/src/jvmMain/kotlin/com/epam/drill/agent/logging/LoggingConfiguration.kt
+++ logging/src/jvmMain/kotlin/com/epam/drill/agent/logging/LoggingConfiguration.kt
@@ -97,7 +97,7 @@ actual object LoggingConfiguration {
     private fun <T : OutputStreamAppender<ILoggingEvent>> configureOutputStreamAppender(appender: T) = appender.apply {
         val context = (LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as Logger).loggerContext
         val encoder = PatternLayoutEncoder().also {
-            it.pattern = "%date{yyyy-MM-dd HH:mm:ss.SSS} %-5level [%logger] %maskedMsg%n%throwable"
+            it.pattern = "%date{yyyy-MM-dd HH:mm:ss.SSS} %-5level [%logger] %.-${messageLimit}maskedMsg%n%throwable"
             it.context = context
             it.start()
         }
